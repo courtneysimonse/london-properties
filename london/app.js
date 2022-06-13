3a3d2cb49b76031a6c924ac530b81cc6544f3fac
Courtney Simonse <courtney.r.elder@gmail.com>
2022-06-13 16:27:16 -0400

    update popup for mobile

--- london/app.js
+++ london/app.js
@@ -294,11 +294,17 @@ function initMap() {
       }
 
       // link
-      // if (event.feature.getProperty('link') != "") {
-      //   popupHTML += "<div class='col-12 d-sm-none ms-2'>";
-      //   popupHTML += "<div class='py-2 my-1'><a class='link-light' target='_blank' href='"+event.feature.getProperty('link')+"'>Learn more...</a></div>";
-      //   popupHTML += "</div>"
-      // }
+      if (event.feature.getProperty('documents') != "") {
+        popupHTML += "<div class='col-12 d-sm-none ms-2'>";
+        popupHTML += "Documents:";
+        let documents = JSON.parse(event.feature.getProperty('documents'));
+        console.log(documents);
+        documents.forEach((item, i) => {
+          popupHTML += "<a target='_blank' class='link-light' href='./documents/"+item[1]+"'>"+item[0]+"</a> ";
+        });
+
+        popupHTML += "</div>"
+      }
 
       popupHTML += "<div class='col-4 d-none d-sm-block px-0 position-relative'>";
       // popupHTML += "<div class='col-md-6 col-xs-6'>";
